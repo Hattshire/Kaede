8824bdf26a664a179253aa2923445d1c840c1f76
Hattshire <Hattshire@users.noreply.github.com>
2017-12-07 15:21:40 -0300

    Search feature added

--- main.py
+++ main.py
@@ -7,14 +7,22 @@ from io import BytesIO
 
 def on_button_clicked( widget ):
 	print("Hi Boi!! <3")
-	Image.open(BytesIO(get_image(get_posts()[0]))).save("sample.jpg")
-	
+	Image.open(BytesIO(get_image(search(["asdf",])[0]))).save("sample.jpg")
+
+def do_search( widget ):
+	data = search( widget.get_text().split( ' ' ) )
+	for item in data:
+		image_byte_stream = BytesIO( get_image(item) )
+		image = Image.open( image_byte_stream )
+		image.save( item['image'] )
+
 def _url( key, data = None ):
 	tail = { 
 			'list'     : "&s=post",
 			'comments' : "&s=comment",
 			}
-	return "http://tbib.org/index.php?page=dapi&json=1" + "&q=index" + tail[ key ]
+	return "http://tbib.org/index.php?page=dapi&json=1" + "&q=index" + tail[ key ] + "&" + str(data)
+
 def _image_url(post_data):
 	return "http://tbib.org//images/" + post_data['directory'] + '/' + post_data['image']
 
@@ -25,15 +33,27 @@ def get_image(post_data):
 	return requests.get( _image_url(post_data) ).content
 
 def search(tags):
-	pass
+	tag_string = "tags=" + '+'.join(tags)
+	return requests.get( _url( 'list', tag_string ) ).json()
 
-win = Gtk.Window( title="Hello boi!!" )
-win.connect( 'delete-event', Gtk.main_quit )
 
-vuvutton = Gtk.Button( label='Click Me! <3' )
-vuvutton.connect( "clicked", on_button_clicked )
+if __name__ == '__main__':
+	win = Gtk.Window( title="Hello boi!!" )
+	win.connect( 'delete-event', Gtk.main_quit )
 
-win.add( vuvutton )
-win.show_all()
+	box = Gtk.Box()
 
-Gtk.main()
+	inputter = Gtk.Entry()
+	inputter.set_icon_from_icon_name(Gtk.EntryIconPosition.PRIMARY, "system-search-symbolic")
+	inputter.connect( "activate", do_search )
+
+	vuvutton = Gtk.Button( label='Click Me! <3' )
+	vuvutton.connect( "clicked", on_button_clicked )
+
+	box.pack_start(inputter, True, True, 0)
+	box.pack_start(vuvutton, True, True, 0)
+
+	win.add( box )
+	win.show_all()
+
+	Gtk.main()
