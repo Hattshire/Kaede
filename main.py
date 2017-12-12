308387c5088b8e083c27a321645d40bb1c15df7b
Hattshire <Hattshire@users.noreply.github.com>
2017-12-12 20:43:27 -0300

    Use classes

--- main.py
+++ main.py
@@ -6,67 +6,100 @@ from PIL import Image
 from io import BytesIO
 from threading import Thread
 
-def do_search( widget, container ):
-	data = search( widget.get_text().split( ' ' ) )
-	Thread(target=add_results,args=[container,data]).start()
+class ImageWidget(Gtk.EventBox):
+	def __init__(self, pixbuf, item_data):
+		super(ImageWidget, self).__init__()
+		if(pixbuf == None):
+			return None
 
-def image_widget_draw(widget,cairo_context,pixbuf):
-	width = widget.get_allocated_width()
-	height = widget.get_allocated_height()
-	scaled_pixbuf = pixbuf.scale_simple(width,height,GdkPixbuf.InterpType.BILINEAR)
-	Gdk.cairo_set_source_pixbuf(cairo_context,scaled_pixbuf,0,0)
-	cairo_context.paint()
+		self.pixbuf = pixbuf
+		self.image = Gtk.Image.new_from_pixbuf(self.pixbuf)
+		self.image.show()
+		self.add(self.image)
+		self.connect("button_press_event",self.image_on_click,item_data)
 
-def image_on_click(widget,event_button,data):
-	popup_window = Gtk.Window()
-	Thread(target=ioc_thread,args=[popup_window,data]).start()
+	def image_on_click(self, widget,event_button,data):
+		popup_window = ImageWindow()
+		Thread(target=popup_window.ioc_thread,args=[popup_window,data]).start()
 
-def ioc_thread(popup_window,data):
-	image_data = get_image(data)
-	pixbuf_loader = GdkPixbuf.PixbufLoader.new()
-	pixbuf_loader.write(image_data)
-	image_pixbuf = pixbuf_loader.get_pixbuf()
-	if(image_pixbuf == None):
-		return
-	image_widget = Gtk.DrawingArea()
-	image_widget.set_hexpand(True)
-	image_widget.set_vexpand(True)
-	image_widget.connect("draw", image_widget_draw,image_pixbuf)
-	image_widget_container = Gtk.Grid()
-	image_widget_container.attach(image_widget,0,0,1,1)
-	Gdk.threads_enter()
-	popup_window.set_default_size( image_pixbuf.get_width(), image_pixbuf.get_height())
-	popup_window.add(image_widget_container)
-	popup_window.show_all()
-	Gdk.threads_leave()
+class MainWindow(Gtk.Builder):
+	def __init__(self):
+		super(MainWindow, self).__init__()
+		self.add_from_file("main_window_ui.glade")
+		self.window = self.get_object("window1")
+		self.window.set_title("Hello boi!!" )
+		self.window.connect( 'delete-event', Gtk.main_quit )
 
-def add_results(container, data):
-	width, height = container.get_size()
-	y = 0
-	x = 0
-	for item in data:
-		image_data = get_thumbnail(item)
+		self.layyout = self.get_object("layout1")
+
+		self.inputter = self.get_object("entry1")
+		self.inputter.set_icon_from_icon_name(Gtk.EntryIconPosition.PRIMARY, "system-search-symbolic")
+		self.inputter.connect( "activate", self.do_search, self.layyout )
+
+	def do_search( self, widget, container ):
+		Thread(target=self.add_results,args=[container,widget.get_text().split( ' ' )]).start()
+
+	def add_results( self, container, tags):
+		data = search( tags )
+		width, height = container.get_size()
+		y = 0
+		x = 0
+		for item in data:
+			image_data = get_thumbnail(item)
+			pixbuf_loader = GdkPixbuf.PixbufLoader.new()
+			pixbuf_loader.write(image_data)
+
+			image_pixbuf = pixbuf_loader.get_pixbuf()
+			if(image_pixbuf == None):
+				continue
+
+			image_event_widget = ImageWidget(image_pixbuf, item)
+			image_event_widget.show()
+
+			if (y+image_pixbuf.get_height() > height):
+				y = 0
+				x += 160
+			Gdk.threads_enter()
+			if (x+160 > width):
+				width += 160
+				container.set_size(width,height,)
+			container.put(image_event_widget,x,y)
+			Gdk.threads_leave()
+			y += image_pixbuf.get_height()
+
+	def show_all(self):
+		self.window.show_all()
+
+class ImageWindow(Gtk.Window):
+	def image_widget_draw(self,widget,cairo_context,pixbuf):
+		width = widget.get_allocated_width()
+		height = widget.get_allocated_height()
+		scaled_pixbuf = pixbuf.scale_simple(width,height,GdkPixbuf.InterpType.BILINEAR)
+		Gdk.cairo_set_source_pixbuf(cairo_context,scaled_pixbuf,0,0)
+		cairo_context.paint()
+
+	def ioc_thread(self, popup_window,data):
+		image_data = get_image(data)
 		pixbuf_loader = GdkPixbuf.PixbufLoader.new()
 		pixbuf_loader.write(image_data)
 		image_pixbuf = pixbuf_loader.get_pixbuf()
 		if(image_pixbuf == None):
-			continue
-		image_widget = Gtk.Image.new_from_pixbuf(image_pixbuf)
-		image_widget.show()
-		image_event_widget = Gtk.EventBox()
-		image_event_widget.add(image_widget)
-		image_event_widget.connect("button_press_event",image_on_click,item)
-		image_event_widget.show()
-		if (y+image_pixbuf.get_height() > height):
-			y = 0
-			x += 160
+			return
+		image_widget = Gtk.DrawingArea()
+		image_widget.set_hexpand(True)
+		image_widget.set_vexpand(True)
+		image_widget.connect("draw", self.image_widget_draw,image_pixbuf)
+		image_widget_container = Gtk.Grid()
+		image_widget_container.attach(image_widget,0,0,1,1)
 		Gdk.threads_enter()
-		if (x+160 > width):
-			width += 160
-			container.set_size(width,height,)
-		container.put(image_event_widget,x,y)
+		popup_window.set_default_size( image_pixbuf.get_width(), image_pixbuf.get_height())
+		popup_window.add(image_widget_container)
+		popup_window.show_all()
 		Gdk.threads_leave()
-		y += image_pixbuf.get_height()
+
+
+
+
 
 def _url( key, data = None ):
 	tail = { 
@@ -100,19 +133,9 @@ def search(tags):
 
 
 if __name__ == '__main__':
-	builder = Gtk.Builder()
-	builder.add_from_file("main_window_ui.glade")
-	win = builder.get_object("window1")
-	win.set_title("Hello boi!!" )
-	win.connect( 'delete-event', Gtk.main_quit )
+	builder = MainWindow()
 
-	layyout = builder.get_object("layout1")
-
-	inputter = builder.get_object("entry1")
-	inputter.set_icon_from_icon_name(Gtk.EntryIconPosition.PRIMARY, "system-search-symbolic")
-	inputter.connect( "activate", do_search, layyout )
-
-	win.show_all()
+	builder.show_all()
 
 	Gdk.threads_init()
 	Gdk.threads_enter()
