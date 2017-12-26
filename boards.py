d9dc9a611912836459b702ed645cc5ad72bea403
Hattshire <Hattshire@users.noreply.github.com>
2017-12-26 13:11:10 -0300

    Extend providers from a common class

--- boards.py
+++ boards.py
@@ -1,7 +1,36 @@
 import requests
 
 
-class gelbooru_provider():
+class BoardProvider():
+    def _url(self, key, data=None):
+        pass
+
+    def _image_url(self, post_data):
+        pass
+
+    def _thumbnail_url(self, post_data):
+        pass
+
+    def get_posts(self):
+        return requests.get(self._url('list')).json()
+
+    def get_image(self, post_data):
+        return requests.get(self._image_url(post_data)).content
+
+    def get_thumbnail(self, post_data):
+        return requests.get(self._thumbnail_url(post_data)).content
+
+    def search(self, tags):
+        tag_string = "tags=" + '+'.join(tags)
+        response = requests.get(self._url('list', tag_string))
+        result = []
+
+        if(response.content):
+            result = response.json()
+        return result
+
+
+class gelbooru_provider(BoardProvider):
     def _url(self, key, data=None):
         tail = {'list': "&s=post",
                 'comments': "&s=comment"}
@@ -18,27 +47,8 @@ class gelbooru_provider():
               post_data['hash'] + '.jpg'
         return url
 
-    def get_posts(self):
-        return requests.get(self._url('list')).json()
 
-    def get_image(self, post_data):
-        return requests.get(self._image_url(post_data)).content
-
-    def get_thumbnail(self, post_data):
-        return requests.get(self._thumbnail_url(post_data)).content
-
-    def search(self, tags):
-        tag_string = "tags=" + '+'.join(tags)
-        response = requests.get(self._url('list', tag_string))
-        result = []
-
-        if(response.content):
-            result = response.json()
-
-        return result
-
-
-class tbib_provider():
+class tbib_provider(BoardProvider):
     def _url(self, key, data=None):
         tail = {'list': "&s=post",
                 'comments': "&s=comment"}
@@ -54,22 +64,3 @@ class tbib_provider():
         url = "http://tbib.org/thumbnails/" + \
               post_data['directory'] + '/thumbnail_' + post_data['image']
         return url
-
-    def get_posts(self):
-        return requests.get(self._url('list')).json()
-
-    def get_image(self, post_data):
-        return requests.get(self._image_url(post_data)).content
-
-    def get_thumbnail(self, post_data):
-        return requests.get(self._thumbnail_url(post_data)).content
-
-    def search(self, tags):
-        tag_string = "tags=" + '+'.join(tags)
-        response = requests.get(self._url('list', tag_string))
-        result = []
-
-        if(response.content):
-            result = response.json()
-
-        return result
