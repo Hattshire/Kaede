343e12ec7b690c4ee38c17ec7437a7cca11f7617
Hattshire <Hattshire@users.noreply.github.com>
2017-12-31 18:32:52 -0300

    GLib unused in main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import errno
 import config
 import threads
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk, Gdk, GdkPixbuf, GLib
+from gi.repository import Gtk, Gdk, GdkPixbuf
 
 
 class ThumbnailWidget(Gtk.EventBox):
