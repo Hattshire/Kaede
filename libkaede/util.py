e72308e6a1909b00232fd9df49b7451240ab2253
Hattshire <hattshire@users.noreply.github.com>
2018-04-08 13:50:40 -0300

    Set async optional.

--- libkaede/util.py
+++ libkaede/util.py
@@ -16,12 +16,20 @@ class Image():
 		self.size = 0
 		self.progress = 0.
 
-	def load(self):
-		"""Load the image."""
-		global WORKER_POOL
-		WORKER_POOL.apply_async(self._loader)
+	def load(self, async=True):
+		"""Load the image.
+
+		Args:
+			async (bool): Load asynchronously.
+		"""
+		if async:
+			global WORKER_POOL
+			WORKER_POOL.apply_async(self._loader)
+		else:
+			self._loader()
 
 	def _loader(self):
+		"""Load the image synchronously."""
 		data_stream = requests.get(self.url, stream=True).raw
 		self.size = data_stream.getheader('Content-Length')
 		for data in data_stream.stream(decode_content=True):
@@ -34,18 +42,22 @@ class Image():
 		"""Remove the image from memory."""
 		self.bytes = b''
 
-	def save(self, folder, filename=None):
-		"""Save the image to disk asynchronously.
+	def save(self, folder, filename=None, async=True):
+		"""Save the image to disk.
 
 		Args:
 			folder (str): Where to save.
 			filename (str): How to name.
+			async (bool): Save asynchronously.
 		"""
-		global WORKER_POOL
-		WORKER_POOL.apply_async(self._save, (folder, filename))
+		if async:
+			global WORKER_POOL
+			WORKER_POOL.apply_async(self._save, (folder, filename))
+		else:
+			self._savefolder, filename)
 
 	def _save(self, folder, filename):
-		"""Save the image to disk.
+		"""Save the image to disk syncronously.
 		
 		Args:
 			folder (str): Where to save.
