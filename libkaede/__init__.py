25746b9b2b01bdfa65db5eb4a01504ff0756bd3a
Hattshire <hattshire@users.noreply.github.com>
2018-06-16 16:51:27 -0400

    TravisCI and details

--- libkaede/__init__.py
+++ libkaede/__init__.py
@@ -29,13 +29,9 @@ gi.require_version('Gtk', '3.0')
 gi.require_version('GLib', '2.0')
 gi.require_version('Gio', '2.0')
 
-APP_NAME = "Kaede"
-APP_VERSION = "0.0.2"
-APP_URL = "https://github.com/Hattshire/Kaede"
-APP_AUTHOR = "Hattshire"
-APP_AUTHOR_MAIL = "com.gmail:hattshire"
 
 WORKER_POOL = ThreadPool()
 
+from .__version__ import *
 from .boards import *
 from .posts import *
---
+++ libkaede/__version__.py
@@ -0,0 +1,8 @@
+__title__ = 'libkaede'
+__description__ = 'Imageboard library for you <3'
+__url__ = 'https://github.com/Hattshire/Kaede'
+__version__ = '0.0.2'
+__author__ = 'Oliver Hattshire'
+__author_email__ = 'com.gmail:hattshire'
+__license__ = 'GPLv3'
+__copyright__ = 'Copyright 2018 Oliver Hattshire'
