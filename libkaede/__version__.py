3911118d4287c92f50770b66d271a59d00c37d97
Hattshire <hattshire@users.noreply.github.com>
2018-06-16 20:10:01 -0400

    Modify version according to PEP440

--- libkaede/__version__.py
+++ libkaede/__version__.py
@@ -1,7 +1,7 @@
 __title__ = 'libkaede'
 __description__ = 'Imageboard library for you <3'
 __url__ = 'https://github.com/Hattshire/Kaede'
-__version__ = '0.0.2'
+__version__ = '0.1.0a0'
 __author__ = 'Oliver Hattshire'
 __author_email__ = 'com.gmail:hattshire'
 __license__ = 'GPLv3'
